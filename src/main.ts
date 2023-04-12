8e8347be3f3e79f1a13ef5feaeb9e678dd4e98dc
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-12 00:43:13 -0300

    chore: add and configure bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '@/assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
