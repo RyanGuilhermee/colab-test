3f660c1a8e018f253f6cbceac197feb9128b4576
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-12 10:15:06 -0300

    feat: adds navbar content and changes component name

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav class="navbar bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">
+      Colab
+    </a>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+</style>
