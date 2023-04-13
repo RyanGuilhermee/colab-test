cf1b89ee0f5a82f41b225e6373e481e2646b2bd3
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-13 13:00:41 -0300

    feat: adds logic to order cards in sequence, implements the api data and search for filters, adds logic to identify the actually page in the button and sends the selected user to modal

--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,46 +1,76 @@
 <script setup lang="ts">
-    import { ref, computed } from "vue";
+    import { ref, computed, defineProps, onMounted, watch } from 'vue';
+    import Modal from '@/components/ModalComponent.vue'
+    import { getUsers, type RandomUser } from '@/services/randomUserService';
 
+    const props = defineProps<{
+        gender: string,
+        nat: string
+    }>();
+
+    let data = ref<RandomUser>({
+        results: [],
+        info: { page: 0, results: 0, seed: '', version: ''}
+    });
     let page = ref(1);
+    const perPage = 6;
+    let userCard = ref({});
 
-    const data = Array.from(Array(50).keys()).map((item) => {
-        return { index: item, value: `this_${item}` };
+    watch([() => props.gender, () => props.nat], async ([newGender, newNat]) => {
+        data.value = await getUsers(`gender=${newGender}&nat=${newNat}`);
     });
 
-    const perPage = 9;
-
+    onMounted(async () => {
+        data.value = await getUsers();
+    });
+    
     const paginatedData = computed(() =>
-        data.slice((page.value - 1) * perPage, page.value * perPage)
+        data.value.results.slice((page.value - 1) * perPage, page.value * perPage)
     );
-
     const nextPage = () => {
-        if (page.value !== Math.ceil(data.length / perPage)) {
+        if (page.value !== Math.ceil(data.value.results.length / perPage)) {
             page.value += 1;
         }
     };
-
     const backPage = () => {
         if (page.value !== 1) {
             page.value -= 1;
         }
     };
-
     const goToPage = (numPage: number) => {
         page.value = numPage;
     };
+    const handleUser = (user: any) => {
+        userCard.value = user;
+    }
 </script>
 
 <template>
+    <Modal :user="userCard" />
     <div class="container text-center">
-        <div v-for="i in 3" :key="i" class="row">
+        <div v-for="i in 2" :key="i" class="row">
             <template v-for="n in 3" :key="n">
-                <div class="col col-md">
-                    <div class="card shadow p-1 mb-5 bg-body-tertiary rounded" style="width: 14rem; height: ;">
-                    <img src="https://randomuser.me/api/portraits/men/75.jpg" class="card-img-top" alt="..."  style="height: 160px;">
+                <div class="col col-md-4">
+                    <div class="card shadow p-1 mb-5 bg-body-tertiary rounded" 
+                    @click="() => handleUser(paginatedData[(i - 1) * 3 + (n - 1)])" 
+                    :id="String((i - 1) * 3 + (n - 1))" 
+                    style="width: 14rem;"
+                    >
+                    <img :src="
+                        paginatedData[(i - 1) * 3 + (n - 1)] ? paginatedData[(i - 1) * 3 + (n - 1)].picture.large :
+                        '...'" class="card-img-top" alt="..."  style="height: 160px;">
                     <div class="card-body">
-                        <h5 class="card-title">{{ paginatedData[(i - 1) * 3 + (n - 1)].value }}</h5>
-                        <p class="card-text">Some quick example text to</p>
-                        <a href="#" class="btn btn-primary">Detalhes</a>
+                        <h5 class="card-title" style="font-size: 17px;">{{ 
+                         paginatedData[(i - 1) * 3 + (n - 1)] ? `${paginatedData[(i - 1) * 3 + (n - 1)].name.first} ${paginatedData[(i - 1) * 3 + (n - 1)].name.last }` :
+                         'Sem elemento'
+                        }}</h5>
+                        <p class="card-text">
+                            {{ paginatedData[(i - 1) * 3 + (n - 1)] ? `${paginatedData[(i - 1) * 3 + (n - 1)].location.country}` :
+                         'Sem elemento' }}
+                        </p>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userDetailsModal">
+                            Detalhes
+                        </button>
                     </div>
                     </div>
                 </div>
@@ -50,8 +80,8 @@
 
     <div class="mt-4 mb-4">
         <button class="btn btn-outline-dark" @click="backPage">prev</button>
-        <button class="btn btn-outline-primary"
-        v-for="item in Math.ceil(data.length / perPage)"
+        <button :class="item === page ? 'btn btn-primary' : 'btn btn-outline-primary'"
+        v-for="item in Math.ceil(data.results.length / perPage)"
         :key="item"
         @click="() => goToPage(item)"
         >
