be702d362747ff521774502596fc304d637c4b4d
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-13 14:41:30 -0300

    feat: implements the user details

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import type { RandomUser } from '@/services/randomUserService';
-import { defineProps, watch, ref } from 'vue';
+  import { defineProps, watch, ref } from 'vue';
 
   const props = defineProps<{user: RandomUser}>();
 
@@ -8,27 +8,53 @@ import { defineProps, watch, ref } from 'vue';
 
   watch(() => props.user, (newUser) => {
     userDetails.value = newUser;
+    const userDateBirthday = new Date(userDetails.value.dob.date);
+    userDetails.value.dob.date = userDateBirthday.toLocaleDateString('pt-BR'); 
   });
 </script>
 
 <template>
-    <div class="modal fade" id="userDetailsModal" tabindex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="userDetailsModalLabel"></h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+  <div class="modal fade" id="userDetailsModal" tabindex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="userDetailsModalLabel">{{ `${userDetails?.name.first} ${userDetails?.name.last}` }}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" 
+            :value="`${userDetails?.location.city}, ${userDetails?.location.state}, ${userDetails?.location.country}`" disabled>
+            <label for="floatingInputDisabled">Localização</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" :value="userDetails?.email" disabled>
+            <label for="floatingInputDisabled">Email</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" :value="userDetails?.cell" disabled>
+            <label for="floatingInputDisabled">Celular</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" :value="userDetails?.dob.date" disabled>
+            <label for="floatingInputDisabled">Data de nascimento</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" :value="`${userDetails?.dob.age} anos`" disabled>
+            <label for="floatingInputDisabled">Idade</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInputDisabled" 
+            :value="userDetails?.gender === 'male' ? 'Masculino' : 'Feminino'" disabled>
+            <label for="floatingInputDisabled">Gênero</label>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
