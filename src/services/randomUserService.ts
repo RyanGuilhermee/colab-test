733b4cd45510642b1fc496fa0088b03e10b1b8fa
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-13 13:24:10 -0300

    feat: adds new type

--- src/services/randomUserService.ts
+++ src/services/randomUserService.ts
@@ -1,7 +1,47 @@
 const baseUrl = 'https://randomuser.me/api';
 
 export type RandomUser = {
-  results: any[];
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  dob: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+};
+//login, registered, id
+export type RandomUserResults = {
+  results: RandomUser[];
   info: {
     page: number;
     results: number;
@@ -10,7 +50,7 @@ export type RandomUser = {
   };
 };
 
-export const getUsers = async (query: string = ''): Promise<RandomUser> => {
+export const getUsers = async (query: string = ''): Promise<RandomUserResults> => {
   const response = await fetch(`${baseUrl}/?results=12&${query}`);
   const data = await response.json();
 
