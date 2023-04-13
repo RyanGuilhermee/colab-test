1d81a42df63d261ddca932966c806eb82a6d7fe9
Ryan Guilherme <ryan.guilherme.740@gmail.com>
2023-04-13 13:01:56 -0300

    feat: creates modal component

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { defineProps, watch, ref } from 'vue';
+
+  const props = defineProps<{
+    user: {}
+  }>();
+
+  const userDetails = ref({});
+
+  watch(() => props.user, (newUser) => {
+    userDetails.value = newUser;
+  });
+</script>
+
+<template>
+    <div class="modal fade" id="userDetailsModal" tabindex="-1" aria-labelledby="userDetailsModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="userDetailsModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
+
